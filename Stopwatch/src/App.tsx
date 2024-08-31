fab74f97dab3ec659d5a20d293b473de2d91ade8
KamilFil <kamilfprog@gmail.com>
2024-08-31 13:35:08 +0200

    Merge commit develop into feat/stopwatch-5

--- Stopwatch/src/App.tsx
+++ Stopwatch/src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css'
 import { Stopwatch } from './components/Stopwatch'
-import {TableLap} from "./components/LapTable/LapTable.tsx";
+import { TableLap } from './components/LapTable/LapTable.tsx'
 
 import { StopwatchControl } from './components/StopwatchControl/StopwatchControl.tsx'
 
 export const App = () => {
-  return <>
-    <Stopwatch/>
-    <StopwatchControl />
-    <TableLap/>
+  return (
+    <>
+      <Stopwatch />
+      <StopwatchControl />
+      <TableLap />
+    </>
+  )
 }
